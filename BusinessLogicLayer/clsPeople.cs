d9c3b0b98bc6b983e74b51380c94039987942789
BilalOrabi <bilalorabi99@gmail.com>
2025-08-14 08:22:58 +0300

    Add files via upload

---
+++ BusinessLogicLayer/clsPeople.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+
+namespace BusinessLogicLayer
+{
+    public class clsPeople
+    {
+        public int PersonID { set; get; }
+        public string NationalNo { set; get; }
+        public string FirstName { set; get; }
+        public string SecondName { set; get; }
+        public string ThirdName { set; get; }
+        public string LastName { set; get; }
+        public string Email { set; get; }
+        public string Phone { set; get; }
+        public string Address { set; get; }
+        public int NationalityCountryID { set; get; }
+        public DateTime DateOfBirth { set; get; }
+        public string ImagePath { set; get; }
+
+        public static DataTable GetAllPeople()
+        {
+            return clsPeopleData.GetAllPeople();
+        }
+    }
+}
