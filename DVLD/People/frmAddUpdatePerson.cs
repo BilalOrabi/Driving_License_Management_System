b0fc050565a81e07a8a2713d1a2e144c8b4ab0b5
BilalOrabi <bilalorabi99@gmail.com>
2025-08-25 22:04:26 +0300

    Updating AddUpadteForm

--- DVLD/People/frmAddUpdatePerson.cs
+++ DVLD/People/frmAddUpdatePerson.cs
@@ -7,15 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BusinessLogicLayer;
 
 namespace DVLD.People
 {
     public partial class frmAddUpdatePerson : Form
     {
+        public delegate void DataBackEventHandler(int person_id);
+        public event DataBackEventHandler DataBack;
+        public enum enMode {AddNew = 1 ,Update = 2 }
+        private enMode _Mode;
+        private int _PersonID = -1;
+        private clsPeople _Person;
         public frmAddUpdatePerson()
         {
             InitializeComponent();
+            _Mode = enMode.AddNew;
         }
+
+        public frmAddUpdatePerson(int person_id)
+        {
+            InitializeComponent();
+            this._PersonID = person_id;
+            _Mode = enMode.Update;
+        }
+
 
         private void btnClose_Click(object sender, EventArgs e)
         {
