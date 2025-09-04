863db274ab27902ec4bc6f942120d04c1c20fcc8
BilalOrabi <bilalorabi99@gmail.com>
2025-09-04 07:52:48 +0300

    progressing add\updateform

--- DVLD/People/frmAddUpdatePerson.cs
+++ DVLD/People/frmAddUpdatePerson.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -113,7 +114,98 @@ namespace DVLD.People
             cbCountry.SelectedIndex = cbCountry.FindString(_Person.CountryInfo.CountryName);
         }
 
+        private void _FillPersonObjectWithFieldsData()
+        {
+            _Person.FirstName = txtFirstName.Text.Trim();
+            _Person.SecondName = txtSecondName.Text.Trim();
+            _Person.ThirdName = txtThirdName.Text.Trim();
+            _Person.LastName = txtLastName.Text.Trim();
+            _Person.NationalNo = txtNationalNo.Text.Trim();
+            _Person.Email = txtEmail.Text.Trim();
+            _Person.Address = txtAddress.Text.Trim();
+            _Person.Phone = txtPhone.Text.Trim();
 
+            _Person.Gendor = (rbMale.Checked) ? (byte)clsPeople.enGendor.Male : (byte)clsPeople.enGendor.Female;
+
+            _Person.DateOfBirth = dtpDateOfBirth.Value;
+
+            _Person.NationalityCountryID = clsCountry.Find(cbCountry.Text).ID;
+
+            if (pbPersonImage.ImageLocation != null)
+                _Person.ImagePath = pbPersonImage.ImageLocation;
+            else
+                _Person.ImagePath = "";
+        }
+        private void _LoadData()
+        {
+            _Person = clsPeople.Find(_PersonID);
+
+            if (_Person == null)
+            {
+                MessageBox.Show("No Person with ID = " + _PersonID, "Person Not Found",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                this.Close();
+                return;
+            }
+
+            _FillFieldsWithPersonInfo();
+
+            //load person image in case it was set.
+            if (_Person.ImagePath != "")
+                pbPersonImage.ImageLocation = _Person.ImagePath;
+
+            //hide/show the remove link in case there is no image for the person
+            llRemoveImage.Visible = (_Person.ImagePath != "");
+        }
+
+        private bool _HandlePersonImage()
+        {
+            // this procedure will handle the person image,
+            // it will take care of deleting the old image from the folder
+            // in case the image changed, and it will rename the new image with guid and 
+            // place it in the images folder.
+
+            // _Person.ImagePath contains the old Image, we check if it changed then we copy the new image
+            if (_Person.ImagePath != pbPersonImage.ImageLocation)
+            {
+
+                if (_Person.ImagePath != "")
+                {
+                    // first we delete the old image from the folder in case there is any.
+                    try
+                    {
+                        File.Delete(_Person.ImagePath);
+                    }
+                    catch (IOException)
+                    {
+                        // We could not delete the file.
+                        // log it later   
+                    }
+                }
+
+                if (pbPersonImage.ImageLocation != null)
+                {
+                    // then we copy the new image to the image folder after we rename it
+                    string SourceImageFile = pbPersonImage.ImageLocation.ToString();
+
+                    //if (clsUtil.CopyImageToProjectImagesFolder(ref SourceImageFile))
+                    //{
+                    //    pbPersonImage.ImageLocation = SourceImageFile;
+
+                    //    return true;
+                    //}
+                    //else
+                    //{
+                    //    MessageBox.Show("Error Copying Image File", "Error",
+                    //        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    //    return false;
+                    //}
+                }
+            }
+
+            return true;
+        }
 
         private void btnClose_Click(object sender, EventArgs e)
         {
