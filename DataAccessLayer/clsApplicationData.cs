16cbfc0cd2b6c8df84be3cb1464c3011a148be87
BilalOrabi <bilalorabi99@gmail.com>
2025-10-01 08:15:12 +0300

    Finishing the Applications Logic Layer

--- DataAccessLayer/clsApplicationData.cs
+++ DataAccessLayer/clsApplicationData.cs
@@ -110,6 +110,65 @@ namespace DataAccessLayer
 
         }
 
+        public static int AddNewApplication(int ApplicantPersonID, DateTime ApplicationDate, int ApplicationTypeID,
+     byte ApplicationStatus, DateTime LastStatusDate,
+     float PaidFees, int CreatedByUserID)
+        {
+
+            //this function will return the new person id if succeeded and -1 if not.
+            int ApplicationID = -1;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"INSERT INTO Applications ( 
+                            ApplicantPersonID,ApplicationDate,ApplicationTypeID,
+                            ApplicationStatus,LastStatusDate,
+                            PaidFees,CreatedByUserID)
+                             VALUES (@ApplicantPersonID,@ApplicationDate,@ApplicationTypeID,
+                                      @ApplicationStatus,@LastStatusDate,
+                                      @PaidFees,   @CreatedByUserID);
+                             SELECT SCOPE_IDENTITY();";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("ApplicantPersonID", @ApplicantPersonID);
+            command.Parameters.AddWithValue("ApplicationDate", @ApplicationDate);
+            command.Parameters.AddWithValue("ApplicationTypeID", @ApplicationTypeID);
+            command.Parameters.AddWithValue("ApplicationStatus", @ApplicationStatus);
+            command.Parameters.AddWithValue("LastStatusDate", @LastStatusDate);
+            command.Parameters.AddWithValue("PaidFees", @PaidFees);
+            command.Parameters.AddWithValue("CreatedByUserID", @CreatedByUserID);
+
+
+
+
+            try
+            {
+                connection.Open();
+
+                object result = command.ExecuteScalar();
+
+                if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                {
+                    ApplicationID = insertedID;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error: " + ex.Message);
+
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+
+
+            return ApplicationID;
+        }
+
         public static bool UpdateApplication(int ApplicationID, int ApplicantPersonID, DateTime ApplicationDate, int ApplicationTypeID,
              byte ApplicationStatus, DateTime LastStatusDate,
              float PaidFees, int CreatedByUserID)
